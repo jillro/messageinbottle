8fd51d5cf94acbb5c659b2f2daaea01ab0e514f1
Jill Royer <hello@jillroyer.me>
2019-12-04 23:56:24 +0100

    feat: messenger webhook basic handler

--- src/app.py
+++ src/app.py
@@ -13,9 +13,14 @@ import messages
 
 logger = logging.getLogger(__name__)
 
+FB_VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
+FB_APP_ID = os.environ.get("FB_APP_ID")
+FB_APP_SECRET = os.environ.get("FB_APP_SECRET")
+FB_PAGE_ID = os.environ.get("FB_PAGE_ID")
+FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN")
+
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 TELEGRAM_API = "https://api.telegram.org/bot" + TELEGRAM_TOKEN + "/"
-
 
 dynamodb = boto3.resource("dynamodb", region_name="eu-west-3")
 messages_table = dynamodb.Table("messageinabottle_messages")
@@ -60,7 +65,28 @@ def lambda_handler(event, context):
     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
+    if event["resource"].startswith("/telegram"):
+        return telegram_handler(event)
 
+    if event["resource"].startswith("/facebook-messenger"):
+        return messenger_handler(event)
+
+
+def messenger_handler(event):
+    qs = event["queryStringParameters"]
+
+    if "hub.mode" in qs and "hub.verify_token" in qs:
+        if qs["hub.mode"] == "subscribe" and qs["hub.verify_token"] == FB_VERIFY_TOKEN:
+            logger.info("Messenger webhook verification success")
+            return {"statusCode": 200, "body": qs["hub.challenge"]}
+        else:
+            logger.error("Messenger webhook verification failure")
+            return {"statusCode": 403}
+
+    logger.error("Messenger webhook not handled")
+
+
+def telegram_handler(event):
     update = json.loads(event["body"])
 
     if "message" not in update:
