f042f44eaea4dae4c41c2bf330d477fea511d56d
Jill Royer <hello@jillroyer.me>
2019-12-08 14:24:23 +0100

    feat: messenger mimic messages

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import hmac
 import json
 import logging
 import os
@@ -26,6 +27,7 @@ dynamodb = boto3.resource("dynamodb", region_name="eu-west-3")
 messages_table = dynamodb.Table("messageinabottle_messages")
 
 OK_RESPONSE = {"statusCode": 200}
+FORDIDDEN_RESPONSE = {"statusCode": 403}
 
 
 def extract_and_sort_hashtags(text, default):
@@ -37,7 +39,7 @@ def extract_and_sort_hashtags(text, default):
     return " ".join(tags)
 
 
-def reply_message(message, text, **kwargs):
+def reply_telegram_message(message, text, **kwargs):
     res = None
     try:
         res = requests.post(
@@ -47,6 +49,24 @@ def reply_message(message, text, **kwargs):
                 "text": text,
                 "reply_to_message_id": message["message_id"],
                 **kwargs,
+            },
+        )
+        res.raise_for_status()
+    except HTTPError as e:
+        if res is not None:
+            logger.error(res.text)
+        raise e
+
+
+def reply_messenger_message(message_event, text, **kwargs):
+    res = None
+    try:
+        res = requests.post(
+            "https://graph.facebook.com/v2.6/me/messages",
+            params={"access_token": FB_PAGE_TOKEN},
+            json={
+                "recipient": {"id": message_event["sender"]["id"]},
+                "message": {"text": text},
             },
         )
         res.raise_for_status()
@@ -80,15 +100,40 @@ def lambda_handler(event, context):
 def messenger_handler(event):
     qs = event["queryStringParameters"]
 
-    if "hub.mode" in qs and "hub.verify_token" in qs:
+    if qs is not None and "hub.mode" in qs and "hub.verify_token" in qs:
         if qs["hub.mode"] == "subscribe" and qs["hub.verify_token"] == FB_VERIFY_TOKEN:
             logger.info("Messenger webhook verification success")
             return {"statusCode": 200, "body": qs["hub.challenge"]}
         else:
             logger.error("Messenger webhook verification failure")
-            return {"statusCode": 403}
+            return FORDIDDEN_RESPONSE
 
-    logger.error("Messenger webhook not handled")
+    expected_signature = str(event["headers"]["X-Hub-Signature"])
+    signature = (
+        "sha1="
+        + hmac.new(
+            FB_APP_SECRET.encode(), event["body"].encode("raw-unicode-escape"), "sha1"
+        ).hexdigest()
+    )
+
+    if not hmac.compare_digest(expected_signature, signature):
+        logger.error(
+            "Messenger webhook failed to authenticate : calculated signature was {}, header signature was {}.".format(
+                signature, expected_signature
+            )
+        )
+        return FORDIDDEN_RESPONSE
+
+    payload = json.loads(event["body"])
+
+    for entry in payload["entry"]:
+        message = entry["messaging"][0]
+        if "message" not in message:
+            logger.error("Webhook type not supported")
+            continue
+        reply_messenger_message(message, message["message"]["text"])
+
+    return OK_RESPONSE
 
 
 def telegram_handler(event):
@@ -103,7 +148,7 @@ def telegram_handler(event):
         return OK_RESPONSE
 
     if message["text"] == "/start":
-        reply_message(message, messages.WELCOME, parse_mode="Markdown")
+        reply_telegram_message(message, messages.WELCOME, parse_mode="Markdown")
         # reply_markup=json.dumps({
         #             "inline_keyboard": [[{"text": "Ok got it !", "callback_data": "/ok"}]],
         #         }))
@@ -124,12 +169,12 @@ def telegram_handler(event):
     )
 
     if len(response["Items"]) == 0:
-        reply_message(message, messages.NO_MESSAGE_EVER)
+        reply_telegram_message(message, messages.NO_MESSAGE_EVER)
 
         return OK_RESPONSE
 
     if message["from"]["id"] == response["Items"][0]["message"]["from"]["id"]:
-        reply_message(message, messages.YOU_AGAIN)
+        reply_telegram_message(message, messages.YOU_AGAIN)
 
         return OK_RESPONSE
 
@@ -153,6 +198,6 @@ def telegram_handler(event):
         messages.MESSAGE_INTRO.format(item["message"]["from"]["first_name"])
         + item["message"]["text"]
     )
-    reply_message(message, text, disable_web_page_preview=True)
+    reply_telegram_message(message, text, disable_web_page_preview=True)
 
     return OK_RESPONSE
