d66fa3c696b6a2fd2cc2bd2d5ddd60e77b43e5d7
Jill Royer <hello@jillroyer.me>
2019-12-29 18:11:13 +0100

    feat: handle dev version for all variables

--- src/settings.py
+++ src/settings.py
@@ -1,13 +1,22 @@
+import logging
 import os
 
-FB_VERIFY_TOKEN = os.environ.get("FB_VERIFY_TOKEN")
-FB_APP_ID = os.environ.get("FB_APP_ID")
-FB_APP_SECRET = os.environ.get("FB_APP_SECRET")
-FB_PAGE_ID = os.environ.get("FB_PAGE_ID")
-FB_PAGE_TOKEN = os.environ.get("FB_PAGE_TOKEN")
-TELEGRAM_TOKEN = (
-    os.environ.get("TELEGRAM_TOKEN")
-    if os.environ.get("PRODUCTION")
-    else os.environ.get("DEV_TELEGRAM_TOKEN")
-)
+DEV = not os.environ.get("PRODUCTION", False)
+
+
+def env(key, default=None):
+    if DEV:
+        key = "DEV_" + key
+    return os.environ.get(key, default)
+
+
+FB_VERIFY_TOKEN = env("FB_VERIFY_TOKEN")
+FB_APP_ID = env("FB_APP_ID")
+FB_APP_SECRET = env("FB_APP_SECRET")
+FB_PAGE_ID = env("FB_PAGE_ID")
+FB_PAGE_TOKEN = env("FB_PAGE_TOKEN")
+TELEGRAM_TOKEN = env("TELEGRAM_TOKEN")
 TELEGRAM_API = "https://api.telegram.org/bot" + TELEGRAM_TOKEN + "/"
+
+if DEV:
+    logging.basicConfig(level=logging.DEBUG)
